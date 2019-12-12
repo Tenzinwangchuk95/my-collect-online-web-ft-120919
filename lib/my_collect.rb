946e56a52433042b59362116acc4851b7dc5c3e3
Tenzin Wangchuk <tenzinwangchuk95@gmail.com>
2019-12-12 05:35:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+  if block_given?
+    empty_array = []
+    counter = 0 
+    while counter < array.length 
+      empty_array << yield(array[counter])
+      counter += 1
+    end
+  else
+    return 0
+  end
+  return empty_array
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  #name.split(" ").first
+end
